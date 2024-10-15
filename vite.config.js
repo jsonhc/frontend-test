5f07cc8969acce2e0d6fcc3419cbce2483dc8a16
jsonhc <json_hc@163.com>
2024-10-15 21:09:16 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,20 @@
-import { defineConfig, loadEnv, ConfigEnv, UserConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
-import { wrapperEnv } from './build'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import { fileURLToPath, URL } from 'node:url';
 
-// 路径查找
-const pathResolve = (dir: string): string => {
-  return resolve(__dirname, '.', dir)
-}
-
-// 设置别名，还可以添加其他路径
-const alias: Record<string, string> = {
-  '@': pathResolve('src'),
-  '@views': pathResolve('src/views'),
-  '@store': pathResolve('src/store/modules'),
-  '@comp': pathResolve('src/components')
-}
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
-  const root = process.cwd()
-  const env = loadEnv(mode, root)
-  const viteEnv = wrapperEnv(env)
-
-  return {
-    base: '/frontend-test/',
-    plugins: [vue()],
-    resolve: {
-      alias
-    },
-    server: {
-      host: '0.0.0.0',
-      port: viteEnv.VITE_PORT,
-      open: true,
-      // 本地跨域代理 https://cn.vitejs.dev/config/server-options.html#server-proxy
-      proxy: {
-        '^/api': {
-          target: 'http://192.168.1.4:8688',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, '')
-        }
-      }
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   }
-})
+});
