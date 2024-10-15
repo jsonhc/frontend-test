246320e543eb1f33c8619d69ff37170e641a49ba
Franz-Pei <pzqfranz@gmail.com>
2024-10-15 20:55:05 +1100

    update the vite

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,13 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
-  base: import.meta.env.MODE === 'production'
-  ? '/test/'  // 替换为你生产环境的路径
-  : '/',
+  // 使用 Vite 的环境变量接口来设置 base
+  base: import.meta.env.PROD ? '/test/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
