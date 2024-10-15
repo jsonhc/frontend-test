092d3e12fd3c3a39baa5025d21ab0cbe7332d695
jsonhc <json_hc@163.com>
2024-10-15 18:59:58 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,18 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
+import { defineConfig, loadEnv, ConfigEnv, UserConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import { wrapperEnv } from './build'
 
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
   const root = process.cwd()
   const env = loadEnv(mode, root)
   const viteEnv = wrapperEnv(env)
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+
   return {
-    base: viteEnv.VITE_PUBLIC_PATH
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: viteEnv.VITE_PUBLIC_PATH,
+    plugins: [vue()],
+    resolve: {
+      alias
     }
   }
-});
+})
