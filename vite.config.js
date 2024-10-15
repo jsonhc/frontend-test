668b9db3d2e443f5dbb58ef0e7385e3989bd9d15
Franz-Pei <pzqfranz@gmail.com>
2024-10-15 20:24:41 +1100

    a new route should set

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,11 +9,12 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  
+  base: import.meta.env.MODE === 'production'
+  ? '/test/'  // 替换为你生产环境的路径
+  : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
-
+});
