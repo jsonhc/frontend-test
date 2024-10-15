55ecb6b5592caec05a1c7d6414b1105ec7fb2b2f
jsonhc <json_hc@163.com>
2024-10-15 22:12:05 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+  base: "/frontend-test/",
   plugins: [
     vue(),
     vueDevTools(),
